65811a71d5abe0a0698d834be0e2bbc9bcbb27d5
yurii2007 <yurash862@gmail.com>
2025-08-07 21:27:38 +0300

    [nvim] cursor

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,8 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- theme
-vim.cmd.colorscheme("satsuki_dark")
+vim.cmd.colorscheme 'zero_two_dark_obsidian'
 
 vim.o.number = true
 vim.o.relativenumber = true
@@ -12,19 +12,20 @@ vim.opt.smartcase = true
 
 -- visual settings
 vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "100"
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '100'
 vim.opt.showmatch = true
 vim.opt.showmode = false
+vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait900-blinkoff400-blinkon250-Cursor/lCursor'
 
 -- file handling
 vim.opt.autoread = true
 vim.opt.autowrite = false
 
 -- behavior
-vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard:append 'unnamedplus'
 vim.opt.modifiable = true
 
 -- disabling netrw for nvim-tree
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
