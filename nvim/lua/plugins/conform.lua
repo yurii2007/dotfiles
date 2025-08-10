e0a6741fc437bb3664f40a645298b38f65b3d5cf
yurii2007 <yurash862@gmail.com>
2025-08-10 19:14:33 +0300

    [nvim] conform prettier config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -19,10 +19,21 @@ return {
       lua = { 'stylua' },
       rust = { 'rustfmt' },
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
     },
     default_format_opts = {
       lsp_format = 'fallback',
     },
     format_on_save = { timeout_ms = 1500 },
+    formatters = {
+      prettierd = {
+        args = {
+          '--stdin-filepath',
+          '$FILENAME',
+        },
+      },
+    },
   },
 }
